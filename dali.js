8c3b7e5156f5ef1684d84d95c1beddf1360f3925
Mathieu Sylvain <mathieu@ti-coco.com>
2011-01-29 23:00:21 -0500

    Renaming and refactoring

--- dali.js
+++ dali.js
@@ -14,7 +14,7 @@
 	this.Dali = Dali;
 
 	function Dali(options) {
-
+		// todo: simplify the dali constructor... less like jQuery
 		/*
 		Root function which returns an already compiled template from its cache
 		*/
@@ -22,36 +22,34 @@
 			return dali.templates[templateId] || null;
 		}
 
-		dali.templates = {};
+		extend(dali, {
+			templates: {},
+			tags: tags,
+			decorators: decorators,
+			version: "0.1"
+		});
 
 		/*
 		Constructor for individual templates
 		*/
 		dali.Template = function Template(id, source, environ, options) {
-
-			this.compile = function() {
-				//console.log("Template source: \n", source);
-				var source = lexer(escape(this.source));
-				return new Function(source);
-			};
-
-			this.render = function render(data, options) {
-				var environ = {};
-				extend(environ, this.environ);
-				extend(environ, {
-					options: options, // todo: see if deep extend is needed here
-					data: data
-				});
-				//console.log("this.render: ", this.handler, this.id, environ, data, options);
-				var output=this.handler.call(environ);
-				//console.log("output2", output);
-				return output;
-			};
-
+			var template = this;
 			this.id = id;
 			this.source = source;
-			this.environ = environ;
-			this.handler = this.compile();
+			this.environ = new Env();
+			this.handler = compile();
+
+			this.render = function render(data) {
+				return this.handler.call(data, this.environ);
+			};
+
+			function compile() {
+				//console.log("Template source: \n", source);
+				var source = lexer(escape(template.source)) +
+						"return env.stream();\n";
+				return new Function("env", source);
+			}
+
 		};
 
 		dali.add = function add(id, source, environParam, optionsParam) {
@@ -64,57 +62,58 @@
 			extend(environ, environParam);
 			dali.templates[id] = new dali.Template(id, source, environ, options);
 		};
+
+		function Env() {
+			this.dali = dali;
+			this.render = function (id, data) {
+				return dali(id).render(data);
+			};
+			this._stream = [];
+			this.out = function (content) {
+				this._stream.push(content);
+			};
+			this.stream = function () {
+				return this._stream.join("");
+			};
+			this.applyTag = function (tagName, args, data, blockHandler) {
+				var content,
+					newEnv = new Env(),
+					tag = tags[tagName];
+				content = tag.apply(data, [args, newEnv, blockHandler]);
+				this.out(content);
+			};
+			this.applyDecorator = function(decoratorName, args) {
+				var str,
+					decorator = decorators[decoratorName],
+					oldArray = this._stream,
+					newArray = [];
+				for (var i in oldArray) {
+					str = decorator.apply(oldArray[i] + "", args);
+					newArray.push(str);
+				}
+				this._stream = newArray;
+			};
+		}
+
 		return dali;
 	}
 
-	function Env(dali) {
-		this.version = "0.1";
-		this.render = function (id, data) {
-			return dali(id).render(data);
-		};
-		this._stream = [];
-		this.out = function (content) {
-			this._stream.push(content);
-		};
-		this.stream = function () {
-			return this._stream.join("");
-		};
-		this.applyTag = function (tagName, args, data, blockHandler) {
-			var content,
-				newEnv = new Env(dali),
-				tag = tags[tagName];
-			content = tag.apply(data, [args, newEnv, blockHandler]);
-			this.out(content);
-		};
-		this.applyDecorator = function(decoratorName, args) {
-			var str,
-				decorator = decorators[decoratorName],
-				oldArray = this._stream,
-				newArray = [];
-			for (var i in oldArray) {
-				str = decorator.apply(oldArray[i] + "", args);
-				newArray.push(str);
-			}
-			this._stream = newArray;
-		};
-	}
 
 	// todo: Refactor: Cut the lexer in smaller functions
 	function lexer(template) {
-		var delimiters,
-			matches,
-			match,
+		var tag,
+			tagsMatch,
 			before,
-			lastMatchStart,
-			lastMatchEnd,
-			args,
+			lastTagStart,
+			lastTagEnd,
 			endSplit,
 			content,
 			segments,
-			argSring,
+			args,
 			decorator,
 			decorators,
 			i,
+			j,
 			tagName,
 			tagType,
 			tree, // a tree representing the tag structure to be rendered
@@ -122,11 +121,8 @@
 			tagNode, // to store newly created tagNodes
 			tagNodePointer; // points to the last tagNode being processed
 
-		delimiters = /{(.*?)}/gm;
-		matches = template.match(delimiters) || [];
-		//console.log("MATCHES: ", matches, template);
-		matches.push(null); // Add a null value to signify the end of the matches
-		lastMatchEnd = 0;
+		tagsMatch = template.match(/{(.*?)}/gm) || [];
+		lastTagEnd = 0;
 		tree = [];
 		treeStack = [];
 
@@ -136,100 +132,89 @@
 		tree.push(tagNode);
 		treeStack.push(tagNode);
 
-		for (i in matches) {
-			if (matches.hasOwnProperty(i)) {
-				match = matches[i];
-				console.log(match);
-				if (!match) {
-					before = template.substring(lastMatchEnd, template.length);
-					if (before.length) {
-						tagNodePointer.children.push(new TagNode("raw", "'" + escape(before) + "'"));
-					}
-				} else {
-					lastMatchStart = template.indexOf(match, lastMatchEnd);
-					before = template.substring(lastMatchEnd, lastMatchStart);
-					if (before.length) {
-						tagNodePointer.children.push(new TagNode("raw", "'" + escape(before) + "'"));
-					}
-					tagName = match.split(" ")[0].substring(1);
-					if (tagName[0] === "/") {
-						tagType = "closeTag";
-						// Remove the trailing brace
-						tagName = tagName.split("}")[0];
-						tagName = tagName.substring(1);
-					} else if (match.substring(match.length -2) === "/}") {
-						tagType = "tag";
-						tagName = tagName.split("/}")[0];
-					} else {
-						tagName = tagName.split("}")[0];
-						tagType = "openTag";
-					}
-
-					// todo: trigger the appropriate tag handler
-					var tagEnd = (tagType === "tag") ? "/}" : "}";
-					content = match.substring(match.indexOf("{")+1, match.lastIndexOf(tagEnd));
-					segments = content.split("&gt;&gt;");
-					if (segments[0].indexOf(" ") > -1) {
-						argSring = segments[0].substring(segments[0].indexOf(" "));
-					} else {
-						argSring = ""
-					}
-					decorators = segments.slice(1);
-
-					//console.log("tagToken: ", tagTokenType, tagToken, match, decorators);
-					//console.log("content: ", content);
-
-					// opening a new scope
-					if (typeof(tags[tagName])!=="function")
-						throw("Statement [" + tagName + "] cannot be parsed!");
-					if (tagType === "tag") {
-						tagNode = new TagNode(tagName, argSring);
-						tagNodePointer.children.push(tagNode);
-					} else if (tagType === "openTag" || tagType === "closeTag") {
-						// todo: refactor: make statementToken and tagToken the same var
-						if (tagType === "openTag") {
-							tagNode = new TagNode(tagName, argSring);
-							tagNodePointer.children.push(tagNode);
-							treeStack.push(tagNode);
-							tagNodePointer = tagNode;
-						} else {
-							if (tagName!==treeStack[treeStack.length-1].name)
-								throw("wrong end of scope!");
-							treeStack.pop();
-							tagNodePointer = treeStack[treeStack.length-1];
-						}
-					}
-
-					// Process chained decorators
-					var targetNode;
-					for (i in decorators) {
-						decorator = decorators[i].trim();
-						if (decorator.indexOf("(") > -1) {
-							tagName = decorator.substring(0, decorator.indexOf("("));
-							args = decorator.substring(decorator.indexOf("("));
-							args = "[" + args.substring(1, args.lastIndexOf(")")) + "]";
-						} else {
-							// todo: setup a better and stricter parsing
-							tagName = decorator;
-							args = "[]";
-						}
-						tagNode.decorators.push(new TagNode(tagName, args));
-
-						//console.log("decorator: ", decorator);
-						//console.log("decoratorName: ", decoratorName);
-						//console.log("decoratorArguments: ", decoratorArguments);
-					}
-					// move the cursor forward
-					lastMatchEnd = lastMatchStart + match.length;
+		for (i in tagsMatch) {
+			if (tagsMatch.hasOwnProperty(i)) {
+				tag = tagsMatch[i];
+				lastTagStart = template.indexOf(tag, lastTagEnd);
+				before = template.substring(lastTagEnd, lastTagStart);
+				if (before.length) {
+					tagNodePointer.children.push(new TagNode("raw", "'" + escape(before) + "'"));
 				}
+				tagName = tag.split(" ")[0].substring(1);
+				if (tagName[0] === "/") {
+					tagType = "closeTag";
+					// Remove the trailing brace
+					tagName = tagName.split("}")[0];
+					tagName = tagName.substring(1);
+				} else if (tag.substring(tag.length -2) === "/}") {
+					tagType = "tag";
+					tagName = tagName.split("/}")[0];
+				} else {
+					tagName = tagName.split("}")[0];
+					tagType = "openTag";
+				}
+
+				var tagEnd = (tagType === "tag") ? "/}" : "}";
+				content = tag.substring(tag.indexOf("{")+1, tag.lastIndexOf(tagEnd));
+				segments = content.split("&gt;&gt;");
+				args = "";
+				if (segments[0].indexOf(" ") > -1) {
+					args = segments[0].substring(segments[0].indexOf(" "));
+				}
+				decorators = segments.slice(1);
+
+				//console.log("tagToken: ", tagTokenType, tagToken, match, decorators);
+				//console.log("content: ", content);
+
+				// opening a new scope
+				if (typeof(tags[tagName])!=="function")
+					throw("Statement [" + tagName + "] cannot be parsed!");
+				if (tagType === "tag") {
+					tagNode = new TagNode(tagName, args);
+					tagNodePointer.children.push(tagNode);
+				} else if (tagType === "openTag" || tagType === "closeTag") {
+					if (tagType === "openTag") {
+						tagNode = new TagNode(tagName, args);
+						tagNodePointer.children.push(tagNode);
+						treeStack.push(tagNode);
+						tagNodePointer = tagNode;
+					} else {
+						if (tagName!==treeStack[treeStack.length-1].name)
+							throw("wrong end of scope!");
+						treeStack.pop();
+						tagNodePointer = treeStack[treeStack.length-1];
+					}
+				}
+
+				// Process chained decorators
+				var targetNode;
+				for (j in decorators) {
+					decorator = decorators[i].trim();
+					if (decorator.indexOf("(") > -1) {
+						tagName = decorator.substring(0, decorator.indexOf("("));
+						args = decorator.substring(decorator.indexOf("("));
+						args = "[" + args.substring(1, args.lastIndexOf(")")) + "]";
+					} else {
+						// todo: setup a better and stricter parsing
+						tagName = decorator;
+						args = "[]";
+					}
+					tagNode.decorators.push(new TagNode(tagName, args));
+
+					//console.log("decorator: ", decorator);
+					//console.log("decoratorName: ", decoratorName);
+					//console.log("decoratorArguments: ", decoratorArguments);
+				}
+				// move the cursor forward
+				lastTagEnd = lastTagStart + tag.length;
 			}
 		}
-		//console.dir(tree);
-		return "var Env=this.Env;\n" +
-				"var env = new Env(this.dali);\n" +
-				"var data = this.data;\n" +
-				compileNode(tree[0]) +
-				"return env.stream();\n";
+		// Add a raw tag for the last piece to be renderec or if not tag are found
+		before = template.substring(lastTagEnd, template.length);
+		if (before.length) {
+			tagNodePointer.children.push(new TagNode("raw", "'" + escape(before) + "'"));
+		}
+		return compileNode(tree[0]);
 	}
 
 	function TagNode(name, argString) {
--- dev-test.js
+++ dev-test.js
@@ -27,25 +27,25 @@ with(QUnit) {
 
 	test("Instantiate template", function() {
 		var template;
-		template = "<div id='test'><div>{out data.ok /}</div></div>";
+		template = "<div id='test'><div>{out this.ok /}</div></div>";
 		$(template).dali();
-		equals($.dali("test").source, "<div>{out data.ok /}</div>", "Instantiate using a jQuery selector");
+		equals($.dali("test").source, "<div>{out this.ok /}</div>", "Instantiate using a jQuery selector");
 
-		template = "<div>{out data.ok /}</div>";
+		template = "<div>{out this.ok /}</div>";
 		$.dali.add("test", template);
 		equals($.dali("test").source, template, "Instantiate using string input and an id");
 	});
 
 	test("Rendering templates inside templates", function() {
-		$.dali.add("parseList", "{each data}{out this /}, {/each}");
-		$.dali.add("test", "Items: {render 'parseList', data.fruits /}");
+		$.dali.add("parseList", "{each this}{out this /}, {/each}");
+		$.dali.add("test", "Items: {render 'parseList', this.fruits /}");
 		equals($.dali("test").render(sampleData), "Items: Orange, Banana, Apple, ");
 	});
 
 	module("Expressions");
 
 	test("Simple expressions", function() {
-		$("<div id='test'><div>{out data.ok /}</div></div>").dali();
+		$("<div id='test'><div>{out this.ok /}</div></div>").dali();
 		equals($.dali("test").render(sampleData), "<div>All Ok!</div>", "Wrong output");
 	});
 
@@ -58,15 +58,15 @@ with(QUnit) {
 	// Todo: test comments
 
 	test("each Statement", function() {
-		$("<div id='test'><div>{each data.fruits }{out this /} {/each}</div></div>").dali();
+		$("<div id='test'><div>{each this.fruits }{out this /} {/each}</div></div>").dali();
 		equals($.dali("test").render(sampleData), "<div>Orange Banana Apple </div>");
 
-		$("<div id='test'><div>{each data.loremItems }{out this.label /}={each this.referenceNumbers}{out this /},{/each} - {/each}</div></div>").dali();
+		$("<div id='test'><div>{each this.loremItems }{out this.label /}={each this.referenceNumbers}{out this /},{/each} - {/each}</div></div>").dali();
 		equals($.dali("test").render(sampleData), "<div>lorem ipsum=156,282,133, - dolor sit=8990,387,5822, - amet aridom=2209,849,437, - </div>");
 	});
 
 	test("if Statement", function() {
-		$("<div id='test'><div>{if data.alwaysTrue }True{/if}{if data.alwaysFalse}False{/if}</div></div>").dali();
+		$("<div id='test'><div>{if this.alwaysTrue }True{/if}{if this.alwaysFalse}False{/if}</div></div>").dali();
 		equals($.dali("test").render(sampleData), "<div>True</div>");
 	});
 
@@ -82,18 +82,18 @@ with(QUnit) {
 
 	test("Text manipulation filters", function() {
 
-		$("<div id='test'><div>{out data.ok >> uppercase /}</div></div>").dali();
+		$("<div id='test'><div>{out this.ok >> uppercase /}</div></div>").dali();
 		equals($.dali("test").render(sampleData), "<div>ALL OK!</div>", "uppercase filter");
-		$("<div id='test'><div>{out data.ok >> lowercase /}</div></div>").dali();
+		$("<div id='test'><div>{out this.ok >> lowercase /}</div></div>").dali();
 		equals($.dali("test").render(sampleData), "<div>all ok!</div>", "lowercase filter");
 
-		$("<div id='test'><div>{out data.okWithWhitespaces >> trim /}</div></div>").dali();
+		$("<div id='test'><div>{out this.okWithWhitespaces >> trim /}</div></div>").dali();
 		equals($.dali("test").render(sampleData), "<div>All Ok!</div>", "trim filter");
 
-		$("<div id='test'><div>{if data.alwaysTrue >> uppercase}True{/if}{if data.alwaysFalse}False{/if}</div></div>").dali();
+		$("<div id='test'><div>{if this.alwaysTrue >> uppercase}True{/if}{if this.alwaysFalse}False{/if}</div></div>").dali();
 		equals($.dali("test").render(sampleData), "<div>TRUE</div>", "uppercase filter inside if statement");
 
-		$("<div id='test'><div>{if data.alwaysTrue >> uppercase}True {out}is{/out} True{/if}</div></div>").dali();
+		$("<div id='test'><div>{if this.alwaysTrue >> uppercase}True {out}is{/out} True{/if}</div></div>").dali();
 		equals($.dali("test").render(sampleData), "<div>TRUE IS TRUE</div>", "uppercase filter inside if statement");
 
 	});

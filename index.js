3119ff71d81b3c9a776f694e980c63177d18d180
unknown <pm.moraisgodin@.nurun.com>
2012-03-20 08:10:58 -0400

    add module index

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 module.exports = require("./lib");
+=======
+exports.dali = require(__dirname +"/lib/dali").dali;
+exports.compile = require(__dirname +"/lib/dali").compile;
+>>>>>>> add module index
